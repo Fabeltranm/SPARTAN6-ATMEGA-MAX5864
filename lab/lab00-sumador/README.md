e9a4ee3464ae032a9798ce7c5e99b4abd4dd8aa0
ferney beltran <fabeltranm@unal.edu.co>
2019-08-12 17:54:35 -0500

    add primi

---
+++ lab/lab00-sumador/README.md
@@ -0,0 +1,13 @@
+EJERCICIO 001
+
+
+Simulación:
+
+1 Generar la simulación para el sumador de 1 bit con carry. para ellos en una terminar ejecutar make iversim
+
+construir sumador de 4 bits
+
+2 terminar de construir el archivo sum4bcc.v,  para ello, instanciar cuatro sumadores de 1 bit
+ 
+3 construir el testbench para simular el sumador de 4 bit
+4 Generar la simulación y comprobar el funcionamiento
---
+++ lab/lab00-sumador/src/sum1bcc_primitive_TB.v
@@ -0,0 +1,33 @@
+module sum1bcc_primitive_TB;
+
+  reg x;
+  reg y;
+  reg c;
+  wire out;
+  wire z;
+
+
+sum1bcc_primitive uut(x, y, c,out,z);
+
+
+initial begin
+x=0; y=0; c=0; #3;
+x=0; y=0; c=1; #3;
+x=0; y=1; c=0; #3;
+x=0; y=1; c=1; #3;
+x=1; y=0; c=0; #3;
+x=1; y=0; c=1; #3;
+x=1; y=1; c=0; #3;
+x=1; y=1; c=1; #3;
+
+
+
+end
+
+initial begin: TEST_CASE
+     $dumpfile("sum1bcc_primitive_TB.vcd");
+     $dumpvars(-1, uut);
+     #(200) $finish;
+   end
+
+endmodule //
