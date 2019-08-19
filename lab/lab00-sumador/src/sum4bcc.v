6452d42cea9d41d14ef4540e41ec4aedbc7c3130
ferney alberto beltran molina <Ferney.Beltran@gmail.com>
2019-08-19 00:13:06 -0500

    add

--- lab/lab00-sumador/src/sum4bcc.v
+++ lab/lab00-sumador/src/sum4bcc.v
@@ -6,7 +6,7 @@ module sum4bcc (xi, yi,co,zi);
   output co;
   output [3 :0] zi;
 
-  wire c1,c2,c3
+  wire c1,c2,c3;
   sum1bcc s0 (.A(x[0]), .B(y[0]), .Ci(0),  .Cout(c1) ,.S(z[0]));
   sum1bcc s1 (.A(x[1]), .B(y[1]), .Ci(c1), .Cout(c2) ,.S(z[1]));
   sum1bcc s2 (.A(x[2]), .B(y[2]), .Ci(c2), .Cout(c3) ,.S(z[2]));
