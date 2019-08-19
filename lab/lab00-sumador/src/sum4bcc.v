1a3e2aec702398cb7d9ba858e6dc7022c1726c43
ferney alberto beltran molina <Ferney.Beltran@gmail.com>
2019-08-19 00:12:23 -0500

    add s

--- lab/lab00-sumador/src/sum4bcc.v
+++ lab/lab00-sumador/src/sum4bcc.v
@@ -1,18 +1,16 @@
-module sum4bcc (xi, yi, ci,co,zi);
+module sum4bcc (xi, yi,co,zi);
 
-// completar  para que la descripción funciones
 
   input [3 :0] xi;
   input [3 :0] yi;
-  input  ci;
   output co;
   output [3 :0] zi;
 
-  wire c1
-  sum1bcc s0(.xi(x[0]), .yi(y[0]), .ci(ci), .co(c1) ,.zi(z[0]));
-  // sum1bcc s1 ...
-  // sum1bcc s2 ...
-  // sum1bcc s3 ...
+  wire c1,c2,c3
+  sum1bcc s0 (.A(x[0]), .B(y[0]), .Ci(0),  .Cout(c1) ,.S(z[0]));
+  sum1bcc s1 (.A(x[1]), .B(y[1]), .Ci(c1), .Cout(c2) ,.S(z[1]));
+  sum1bcc s2 (.A(x[2]), .B(y[2]), .Ci(c2), .Cout(c3) ,.S(z[2]));
+  sum1bcc s3 (.A(x[3]), .B(y[3]), .Ci(c3), .Cout(co) ,.S(z[3]));
 
 
 endmodule
