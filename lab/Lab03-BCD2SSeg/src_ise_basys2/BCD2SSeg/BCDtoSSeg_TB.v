67cfad22248dda670842c27933b4bd21fd720841
ferney beltran <fabeltranm@unal.edu.co>
2019-08-26 10:28:41 -0500

    add ejemplo03

---
+++ lab/Lab03-BCD2SSeg/src_ise_basys2/BCD2SSeg/BCDtoSSeg_TB.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+
+
+module BCDtoSSeg_TB;
+
+	// Inputs
+	reg [3:0] BCD;
+
+	// Outputs
+	wire [6:0] SSeg;
+
+	// Instantiate the Unit Under Test (UUT)
+	BCDtoSSeg uut (
+		.BCD(BCD), 
+		.SSeg(SSeg)
+	);
+
+	initial begin
+
+		BCD = 0; #10;
+		BCD = 1; #10;
+		BCD = 2; #10;
+		BCD = 3; #10;
+		BCD = 4; #10;
+		BCD = 5; #10;
+		BCD = 6; #10;
+		BCD = 7; #10;
+		BCD = 8; #10;
+		BCD = 9; #10;
+
+	end
+
+   initial begin: TEST_CASE
+     $dumpfile("BCDtoSSeg_TB.vcd");
+     #(200) $finish;
+   end
+
+endmodule
+
