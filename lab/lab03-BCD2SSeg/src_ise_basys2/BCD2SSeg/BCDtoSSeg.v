807468bd0ec78dcb82c7e43cd4ae89811470a3c2
ferney alberto beltran molina <Ferney.Beltran@gmail.com>
2019-09-09 21:01:31 -0500

    ADD A,B,C

--- lab/lab03-BCD2SSeg/src_ise_basys2/BCD2SSeg/BCDtoSSeg.v
+++ lab/lab03-BCD2SSeg/src_ise_basys2/BCD2SSeg/BCDtoSSeg.v
@@ -5,24 +5,26 @@ module BCDtoSSeg (BCD, SSeg, an);
   output [3:0] an;
 
 assign an=4'b1110;
+
+
 always @ ( * ) begin
   case (BCD)
-    4'h0: SSeg = ~7'b1111110;
-    4'h1: SSeg = ~7'b0110000;
-    4'h2: SSeg = ~7'b1101101;
-    4'h3: SSeg = ~7'b1111001;
-    4'h4: SSeg = ~7'b0110011;
-    4'h5: SSeg = ~7'b1011011;
-    4'h6: SSeg = ~7'b1011111;
-    4'h7: SSeg = ~7'b1110000;
-    4'h8: SSeg = ~7'b1111111;
-    4'h9: SSeg = ~7'b1110011;
-    4'ha: SSeg = ~7'b0011101;
-    4'hb: SSeg = ~7'b0011111;
-    4'hc: SSeg = ~7'b0001101;
-    4'hd: SSeg = ~7'b0111101;
-    4'he: SSeg = ~7'b1001111;
-    4'hf: SSeg = ~7'b1000111;
+   4'b0000: SSeg = 7'b0000001; // "0"  
+	4'b0001: SSeg = 7'b1001111; // "1" 
+	4'b0010: SSeg = 7'b0010010; // "2" 
+	4'b0011: SSeg = 7'b0000110; // "3" 
+	4'b0100: SSeg = 7'b1001100; // "4" 
+	4'b0101: SSeg = 7'b0100100; // "5" 
+	4'b0110: SSeg = 7'b0100000; // "6" 
+	4'b0111: SSeg = 7'b0001111; // "7" 
+	4'b1000: SSeg = 7'b0000000; // "8"  
+	4'b1001: SSeg = 7'b0000100; // "9" 
+   4'ha: SSeg = 7'b0001000;  
+   4'hb: SSeg = 7'b1100000;
+   4'hc: SSeg = 7'b0110001;
+   4'hd: SSeg = 7'b1000010;
+   4'he: SSeg = 7'b0110000;
+   4'hf: SSeg = 7'b0111000;
     default:
     SSeg = 0;
   endcase
